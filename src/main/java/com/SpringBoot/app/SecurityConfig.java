cc52f84d8ba041591b78d715649fad584a6e8998
manuponton <manuealejandro09@gmail.com>
2020-01-04 11:14:03 -0500

    Update SecurityConfig.java

--- src/main/java/com/SpringBoot/app/SecurityConfig.java
+++ src/main/java/com/SpringBoot/app/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
